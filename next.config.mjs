e4858e3996d8b12c8ad87977d6f3ab751fec95eb
develoFavour <opiafavour10@gmail.com>
2025-03-31 22:02:42 +0100

    Profile Image Uploading implementation with vercel/blob

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,17 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	output: "standalone",
+	images: {
+		domains: ["ogteinyzno7ew73x.public.blob.vercel-storage.com"],
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "*.public.blob.vercel-storage.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 
 export default nextConfig;
